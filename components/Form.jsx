28a299ef9c562e5bff4888e729dc4dfcb5b00511
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2024-02-28 13:36:11 +0530

    fix typoasss

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+
 const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
     <section className="w-full max-w-full flex-start flex-col">
@@ -7,8 +8,9 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
       </h1>
       <p className="desc text-left max-w-md">
         {type} and share amazing prompts with the world, and let your
-        imagination run wild with any AI-Powered platform.
+        imagination run wild with any AI-powered platform
       </p>
+
       <form
         onSubmit={handleSubmit}
         className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
@@ -17,39 +19,44 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
           <span className="font-satoshi font-semibold text-base text-gray-700">
             Your AI Prompt
           </span>
+
           <textarea
             value={post.prompt}
             onChange={(e) => setPost({ ...post, prompt: e.target.value })}
-            placeholder="Write Your Prompt here!"
+            placeholder="Write your post here"
             required
-            className="form_textarea"
+            className="form_textarea "
           />
         </label>
+
         <label>
           <span className="font-satoshi font-semibold text-base text-gray-700">
-            Tag{" "}
+            Field of Prompt{" "}
             <span className="font-normal">
-              #product, #webdevelopment, #idea
+              (#product, #webdevelopment, #idea, etc.)
             </span>
           </span>
           <input
             value={post.tag}
             onChange={(e) => setPost({ ...post, tag: e.target.value })}
-            placeholder="#tag"
+            type="text"
+            placeholder="#Tag"
             required
             className="form_input"
           />
         </label>
+
         <div className="flex-end mx-3 mb-5 gap-4">
           <Link href="/" className="text-gray-500 text-sm">
             Cancel
           </Link>
+
           <button
             type="submit"
             disabled={submitting}
             className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white"
           >
-            {submitting ? `${type}...` : type}
+            {submitting ? `${type}ing...` : type}
           </button>
         </div>
       </form>
