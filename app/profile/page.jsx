ad1c9864f062ea25fb4520687b807bf1f1353b81
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2024-02-28 11:26:46 +0530

    CRUD operation done

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,15 +1,35 @@
 "use client";
 
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
 import Profile from "@components/Profile";
 
 const MyProfile = () => {
+  const router = useRouter();
   const { data: session } = useSession();
   const [posts, setPosts] = useState([]);
-  const handleEdit = () => {};
-  const handleDelete = async () => {};
+
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this Prompt?"
+    );
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, { method: "DELETE" });
+        const filteredPosts = post.filter((p) => p._id !== post._id);
+        setPosts(filteredPosts);
+        router.push("/");
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await fetch(`/api/users/${session?.user.id}/posts`);
