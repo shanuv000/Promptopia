ec71279eab904e996f90228de24207aff174c17d
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2024-03-07 14:03:25 +0530

    asdsa

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,24 +1,32 @@
 import React from "react";
-import Heading from "@components/Heading";
+// import Heading from "@components/Heading";
 import "@styles/globals.css";
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 import prompImg from "../promp1.jpg";
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+  image: { prompImg },
+};
 const RootLayout = ({ children }) => {
   return (
     <main>
-      <Heading />
+      {/* <Heading /> */}
       {/* Rest of the layout */}
-      <Provider>
-        <div className="main">
-          <div className="gradient" />
-        </div>
-        <main className="app">
-          <Nav />
-          {children}
-        </main>
-      </Provider>
-      /
+      <html lang="en">
+        <body>
+          <Provider>
+            <div className="main">
+              <div className="gradient" />
+            </div>
+            <main className="app">
+              <Nav />
+              {children}
+            </main>
+          </Provider>
+        </body>
+      </html>
     </main>
   );
 };
--- app/page.jsx
+++ app/page.jsx
@@ -13,7 +13,6 @@ const Home = () => {
         discover, create and share creative prompts
       </p>
 
-      {/* `Feed` */}
       <Feed />
     </section>
   );
