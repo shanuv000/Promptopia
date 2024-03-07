01545985088fc6a3a82d55b45575bf9ced97b5d2
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2024-03-07 14:08:24 +0530

    assad

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,34 +1,28 @@
-import React from "react";
-// import Heading from "@components/Heading";
 import "@styles/globals.css";
+
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
-import prompImg from "../promp1.jpg";
+
 export const metadata = {
   title: "Promptopia",
   description: "Discover & Share AI Prompts",
-  image: { prompImg },
 };
-const RootLayout = ({ children }) => {
-  return (
-    <main>
-      {/* <Heading /> */}
-      {/* Rest of the layout */}
-      <html lang="en">
-        <body>
-          <Provider>
-            <div className="main">
-              <div className="gradient" />
-            </div>
-            <main className="app">
-              <Nav />
-              {children}
-            </main>
-          </Provider>
-        </body>
-      </html>
-    </main>
-  );
-};
+
+const RootLayout = ({ children }) => (
+  <html lang="en">
+    <body>
+      <Provider>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
 
 export default RootLayout;
