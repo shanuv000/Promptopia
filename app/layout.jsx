f7af781fc1bc750c6d8bd0bf8cab04e7b8d50df0
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2024-03-07 14:15:11 +0530

    sadjasdjkb

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,15 +2,13 @@ import "@styles/globals.css";
 
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
-import Heading from "@components/Heading";
-// export const metadata = {
-//   title: "Promptopia",
-//   description: "Discover & Share AI Prompts",
-// };
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
 
 const RootLayout = ({ children }) => (
   <html lang="en">
-    <Heading />
     <body>
       <Provider>
         <div className="main">
